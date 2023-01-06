dea68b2a7b3805f64e4a685647d633c2f3ec897b
Aliaksandr Kunats <kunatswot@gmail.com>
2023-01-06 19:19:53 +0300

    add: package for web pages; controller class that handles requests to the homepage; first internet page "home"

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,11 +3,11 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication   // <1>
+@SpringBootApplication
 public class TacoCloudApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TacoCloudApplication.class, args); // <2>
+    SpringApplication.run(TacoCloudApplication.class, args);
   }
 
 }
