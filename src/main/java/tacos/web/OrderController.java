35fc1119b322ba3229ae647c7ae93dca20ff09e6
Aliaksandr Kunats <kunatswot@gmail.com>
2023-01-10 10:44:28 +0300

    add: the method that handles the dispatch of the order

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,7 +2,10 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.TacoOrder;
 
 import javax.validation.Valid;
 
@@ -15,5 +18,18 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
     }
 }
